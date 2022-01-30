4d2b58dcffc03fbc8b334887e92260f550852738
Archit Nagpal <architnagpal312@gmail.com>
2022-01-30 15:48:08 +0530

    golang data version 9

--- isogram/checkIso.go
+++ isogram/checkIso.go
@@ -3,18 +3,21 @@ package main
 import "fmt"
 
 var s1 string
-func main(){
+var c string
+
+func main() {
 	fmt.Println("Enter a string:")
 	fmt.Scanln(&s1)
+	checkIso()
 }
-func checkIso() bool{
-	for i:=0 ; i < len(s1) ; i++ {
-		for j := 0 ; j < len(s1) ; j++ {
-			if (s1[i] == s1[j]) {
-				return false
+func checkIso() bool {
+	for i := 0; i < len(s1); i++ {
+		for j := 0; j < len(s1); j++ {
+			if s1[i] != s1[j] {
+				c = "false"
 			}
 		}
-		fmt.Println("It is isogram")
 	}
+	fmt.Println(c)
 	return true
 }
